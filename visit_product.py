70aee03f64fc88b7836f6179ff956453e4c2a0ff
niemdinhtrong <niemdinhtrong@gmail.com>
2020-09-08 10:36:18 +0700

    multi thread

--- visit_product.py
+++ visit_product.py
@@ -1,5 +1,7 @@
 import requests
+import queue
 from bs4 import BeautifulSoup
+from threading import Thread
 
 URL = "https://cohami.online/product-category/t-shirts/"
 
@@ -8,18 +10,23 @@ def get_data(url):
     data = BeautifulSoup(datas.text, "lxml")
     return data
 
-def get_product(data):
+def get_product(data,q1):
     datas = data.find(class_="products columns-4")
     datas = datas.find_all('li')
-    return datas
-
-def visit_product(datas):
     for data in datas:
         url = data.find('a').get('href').strip()
+        q1.put(url)
+
+def visit_product(q1):
+    while not q1.empty():
         try:
+            url = q1.get()
             a = requests.get(url)
+            print(url)
+            q.task_done()
         except:
             pass
+
 def get_num_pages(data):
     try:
         datas = data.find(class_="page-numbers")
@@ -40,16 +47,28 @@ def get_num_pages(data):
         num_pages = []
         return num_pages
 
+def task(q):
+    all_proc = []
+    for i in range (4):
+        proc = Thread(target=visit_product, args=(q,))
+        all_proc.append(proc)
+    for p in all_proc:
+        p.start()
+    for p in all_proc:    
+        p.join()
+
+
 def main():
+    q1 = queue.Queue()
     data = get_data(URL)
     num_pages = get_num_pages(data)
-    products = get_product(data)
-    visit_product(products)
+    products = get_product(data, q1)
+    task(q1)
     if (len(num_pages) != 0):
         for page in num_pages:
             data = get_data(URL)
-            products = get_product(data)
-            visit_product(products)
+            products = get_product(data, q1)
+            task(q1)
 
 if __name__ == "__main__":
     main()
